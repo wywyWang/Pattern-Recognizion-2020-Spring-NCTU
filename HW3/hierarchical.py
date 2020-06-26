e3a0b40c2f34b160e8287827cf224de663cdecdd
yao <sf163864@gmail.com>
2020-06-26 16:25:16 +0800

    fix code bug of clustering

--- HW3/hierarchical.py
+++ HW3/hierarchical.py
@@ -3,6 +3,7 @@ import pandas as pd
 import sys
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+from scipy.cluster.hierarchy import dendrogram, linkage
 from sklearn.metrics.pairwise import pairwise_distances
 from sklearn.datasets import make_circles, make_moons
 
@@ -13,7 +14,8 @@ def clustering(data, method):
     clusters[0] = array.copy()
     distances = pairwise_distances(data, metric='euclidean')
     np.fill_diagonal(distances,sys.maxsize)
-    
+    min_row = -1
+    min_col = -1
     for iteration in range(1, distances.shape[0]):
         min_dist = sys.maxsize
         min_row = -1
@@ -48,13 +50,15 @@ def clustering(data, method):
             distances[min_row][update_idx] = sys.maxsize
             distances[update_idx][min_row] = sys.maxsize
 
+        maximun = max(array[min_row], array[min_col])
+        minimun = min(array[min_row], array[min_col])
         for idx in range(len(array)):
-            if array[idx] == max(min_row, min_col):
-                array[idx] = min(min_row, min_col)
+            if array[idx] == maximun:
+                array[idx] = minimun
         clusters[iteration] = array.copy()
-        # print(min_row, min_col, min_dist)
-        # print("Iter: {}, clusters: {}".format(iteration, clusters))
-
+        # print()
+        # print(min_row, '\t', min_col)
+        # print("Iter: {}, clusters: {}".format(iteration, clusters[iteration]))
     return clusters
 
 
@@ -63,13 +67,13 @@ def visualization(data, clusters, link_type, data_type):
     # color = cm.rainbow(np.linspace(0, 1, len(data)))
     color = [np.random.rand(3,) for _ in range(len(data))]
 
-    if data_type == 'circles':
+    if data_type == 'circles' or data_type == 'moons':
         iterations = [_ for _ in range(0, len(clusters), 10)]
         iterations.append(len(clusters) - 2)
         iterations.append(len(clusters) - 1)
+        fig = plt.figure()
         for selected_iteration in iterations:
             #plotting the clusters
-            fig = plt.figure()
             fig.suptitle('Scatter Plot for {} link clusters in iteration {}'.format(link_type, selected_iteration))
             ax = fig.add_subplot(1, 1, 1)
             ax.set_xlabel('X')
@@ -78,6 +82,7 @@ def visualization(data, clusters, link_type, data_type):
                 selected_data = clusters[selected_iteration][data_idx]
                 ax.scatter(data[data_idx, 0], data[data_idx, 1], color=color[selected_data])
             plt.savefig('./{}_{}_link/{}.png'.format(data_type, link_type, selected_iteration))
+            plt.clf()
     else:
         for selected_iteration in range(len(clusters)):
             #plotting the clusters
@@ -90,6 +95,7 @@ def visualization(data, clusters, link_type, data_type):
                 selected_data = clusters[selected_iteration][data_idx]
                 ax.scatter(data[data_idx, 0], data[data_idx, 1], color=color[selected_data])
             plt.savefig('./{}_{}_link/{}.png'.format(data_type, link_type, selected_iteration))
+            plt.clf()
 
 
 if __name__ == "__main__":
@@ -109,9 +115,24 @@ if __name__ == "__main__":
     # visualization(data, clusters, link_type, data_type)
 
 
-    # Data 2
-    data_type = 'circles'
-    data = make_circles(n_samples=100, factor=0.5)
+    # # Data 2
+    # data_type = 'circles'
+    # data = make_circles(n_samples=100, factor=0.5)
+    # link_type = 'single'
+    # clusters = clustering(data[0], link_type)
+    # visualization(data[0], clusters, link_type, data_type)
+
+    # link_type = 'complete'
+    # clusters = clustering(data[0], link_type)
+    # visualization(data[0], clusters, link_type, data_type)
+
+    # link_type = 'average'
+    # clusters = clustering(data[0], link_type)
+    # visualization(data[0], clusters, link_type, data_type)
+
+    # Data 3
+    data_type = 'moons'
+    data = make_moons(n_samples=100)
     link_type = 'single'
     clusters = clustering(data[0], link_type)
     visualization(data[0], clusters, link_type, data_type)
